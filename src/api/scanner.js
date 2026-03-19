67fb46309f83997a4ef75383094041bd4f473ecc
haus4812 <haus4812@gamil.com>
2026-03-19 13:46:40 +0900

    history 수정다시 올림

--- src/api/scanner.js
+++ src/api/scanner.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API_BASE_URL = "http://localhost:8000";
+const API_BASE_URL = "http://localhost:8080";
 
 // 💡 userId 파라미터 추가!
 export const scanMalware = async (payload, type, userId) => { 
